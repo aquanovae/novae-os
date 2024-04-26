40327f680f3e0996ae8b8f4d5a21579232e73a74
RicoProductions <ericravaz@protonmail.com>
2024-04-26 23:04:17 +0200

    add home manager

--- flake.nix
+++ flake.nix
@@ -1,15 +1,27 @@
 {
-  description = "Configuration root";
+  description = "RicOS config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.zenblade = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./configuration.nix
+        ./machines/zenblade/configuration.nix
+
+	home-manager.nixosModules.home-manager {
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+
+	  home-manager.users.rico = import ./home/home.nix;
+	}
       ];
     };
   };
---
+++ home/home.nix
@@ -0,0 +1,6 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "rico";
+  home.homeDirectory = "/home/rico";
+}
