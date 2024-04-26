bc0f21c715b01b9ea689136d9447c8013dcdb0b4
RicoProductions <ericravaz@protonmail.com>
2024-04-26 22:15:33 +0200

    import nix config

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Configuration root";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.zenblade = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
---
+++ hosts/zenblade/configuration.nix
@@ -0,0 +1,165 @@
+{ config, lib, pkgs, ... }: {
+
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      nvidiaSettings = true;
+      prime = {
+        sync.enable = true;
+        amdgpuBusId = "PCI:5:0:0";
+	nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+  };
+
+
+  fileSystems = let options = {
+    options = [ "noatime" "ssd" "discard" ];
+  }; in {
+    "/" = options;
+    "/nix" = options;
+    "/home" = options;
+    "/home/rico/rstore" = options;
+  };
+
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_zen;
+    kernelModules = [
+      "amdgpu"
+      "nvidia"
+      "nvidia_modeset"
+      "nvidia_uvm"
+      "nvidia_drm"
+    ];
+
+    kernelParams = [
+      "quiet"
+      "splash"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "nvidia_drm.modeset=1"
+    ];
+
+    initrd = {
+      systemd.enable = true;
+      luks.devices."root".allowDiscards = true;
+      supportedFilesystems = [ "btrfs" ];
+      verbose = false;
+    };
+    
+    loader = {
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+	efiSupport = true;
+	device = "nodev";
+      };
+    };
+
+    plymouth = {
+      enable = true;
+      theme = "hexagon_hud";
+      themePackages = [
+        pkgs.nixos-bgrt-plymouth
+        (pkgs.adi1090x-plymouth-themes.override {
+	  selected_themes = [ "hexagon_hud" ];
+	})
+      ];
+    };
+
+    consoleLogLevel = 0;
+  };
+
+
+  systemd.services = {
+    "getty@tty1".enable = false;
+    "autovt@tty1".enable = false;
+  };
+
+
+  networking = {
+    hostName = "zenblade";
+    wireless.iwd.enable = true;
+  };
+
+
+  time.timeZone = "Europe/Zurich";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console.keyMap = "fr_CH";
+
+
+  services = {
+    xserver = {
+      enable = true;
+      videoDrivers = [
+        "amdgpu"
+	"nvidia"
+	"nvidia_modeset"
+	"nvidia_uvm"
+	"nvidia_drm"
+      ];
+
+      displayManager = {
+        gdm.enable = true;
+        autoLogin = {
+          enable = true;
+	  user = "rico";
+        };
+      };
+    };
+
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+      alsa = {
+        enable = true;
+	support32Bit = true;
+      };
+    };
+
+    printing.enable = true;
+  };
+
+
+  programs.hyprland.enable = true;
+
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    alacritty
+    bemenu
+    firefox
+    gh
+    git
+    imagemagick
+    neovim
+    swaybg
+    wget
+  ];
+
+
+  users.users.rico = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ];
+     packages = with pkgs; [ ];
+  };
+
+
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
+
