bdd97de5c6e010bccfd54820bde46a7f757165ea
RicoProductions <ericravaz@protonmail.com>
2024-05-09 00:08:18 +0200

    move bindings to top

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -2,12 +2,64 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    #systemd.enable = true;
 
     settings = with config.colors; {
       monitor = "eDP-1, prefered, auto, 1";
 
       exec-once = "swaybg -m fill -i ~/rstore/ricos/home/theme/wallpaper.png";
+
+      input = {
+        bind = [
+          "super, return, exec, alacritty"
+          "super shift, q, killactive"
+          "super, t, togglefloating"
+          "super, o, exec, bemenu-run"
+
+          "super, h, movefocus, l"
+          "super, l, movefocus, r"
+          "super, k, movefocus, u"
+          "super, j, movefocus, d"
+
+          "super, 1, workspace, 1"
+          "super, 2, workspace, 2"
+          "super, 3, workspace, 3"
+          "super, 4, workspace, 4"
+          "super, 5, workspace, 5"
+          "super, 6, workspace, 6"
+          "super, 7, workspace, 7"
+          "super, 8, workspace, 8"
+          "super, 9, workspace, 9"
+          "super, 0, workspace, 10"
+
+          "super shift, 1, movetoworkspace, 1"
+          "super shift, 2, movetoworkspace, 2"
+          "super shift, 3, movetoworkspace, 3"
+          "super shift, 4, movetoworkspace, 4"
+          "super shift, 5, movetoworkspace, 5"
+          "super shift, 6, movetoworkspace, 6"
+          "super shift, 7, movetoworkspace, 7"
+          "super shift, 8, movetoworkspace, 8"
+          "super shift, 9, movetoworkspace, 9"
+          "super shift, 0, movetoworkspace, 10"
+
+          "super, S, togglespecialworkspace, magic"
+          "super shift, S, movetoworkspace, special:magic"
+
+          "super, mouse_down, workspace, e+1"
+          "super, mouse_up, workspace, e-1"
+        ];
+
+        kb_layout = "ch";
+        kb_variant = "fr_nodeadkeys";
+        kb_model = "asus_laptop";
+        numlock_by_default = true;
+
+        follow_mouse = 1;
+
+        touchpad = {
+          natural_scroll = true;
+        };
+      };
 
 
       general = {
@@ -48,61 +100,6 @@
       misc = {
         force_default_wallpaper = 0;
         disable_hyprland_logo = true;
-      };
-
-
-      input = {
-        kb_layout = "ch";
-        kb_variant = "fr_nodeadkeys";
-        kb_model = "asus_laptop";
-
-        follow_mouse = 1;
-
-        touchpad = {
-          natural_scroll = "yes";
-        };
-        
-
-        "$mainMod" = "SUPER";
-        bind = [
-          "$mainMod, return, exec, alacritty"
-          "$mainMod shift, q, killactive"
-          "$mainMod, t, togglefloating"
-          "$mainMod, o, exec, bemenu-run"
-
-          "$mainMod, h, movefocus, l"
-          "$mainMod, l, movefocus, r"
-          "$mainMod, k, movefocus, u"
-          "$mainMod, j, movefocus, d"
-
-          "$mainMod, 1, workspace, 1"
-          "$mainMod, 2, workspace, 2"
-          "$mainMod, 3, workspace, 3"
-          "$mainMod, 4, workspace, 4"
-          "$mainMod, 5, workspace, 5"
-          "$mainMod, 6, workspace, 6"
-          "$mainMod, 7, workspace, 7"
-          "$mainMod, 8, workspace, 8"
-          "$mainMod, 9, workspace, 9"
-          "$mainMod, 0, workspace, 10"
-
-          "$mainMod SHIFT, 1, movetoworkspace, 1"
-          "$mainMod SHIFT, 2, movetoworkspace, 2"
-          "$mainMod SHIFT, 3, movetoworkspace, 3"
-          "$mainMod SHIFT, 4, movetoworkspace, 4"
-          "$mainMod SHIFT, 5, movetoworkspace, 5"
-          "$mainMod SHIFT, 6, movetoworkspace, 6"
-          "$mainMod SHIFT, 7, movetoworkspace, 7"
-          "$mainMod SHIFT, 8, movetoworkspace, 8"
-          "$mainMod SHIFT, 9, movetoworkspace, 9"
-          "$mainMod SHIFT, 0, movetoworkspace, 10"
-
-          "$mainMod, S, togglespecialworkspace, magic"
-          "$mainMod SHIFT, S, movetoworkspace, special:magic"
-
-          "$mainMod, mouse_down, workspace, e+1"
-          "$mainMod, mouse_up, workspace, e-1"
-        ];
       };
     };
   };
