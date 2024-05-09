5ce8b9f005424ff13eaaf05df1cb69e1f458f005
RicoProductions <ericravaz@protonmail.com>
2024-05-09 22:56:47 +0200

    workspace rules and bindings

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -6,13 +6,20 @@
     settings = with config.colors; {
       monitor = "eDP-1, prefered, auto, 1";
 
-      exec-once = "swaybg -m fill -i ~/rstore/ricos/home/theme/wallpaper.png";
+      exec-once = [
+        "swaybg -m fill -i ~/rstore/ricos/home/theme/wallpaper.png"
+        "hyprctl dispatch workspace name:terminal"
+      ];
+
+      workspace = [
+        "name:terminal, default:true, on-created-empty:alacritty"
+        "name:browser, on-created-empty:firefox"
+      ];
 
       input = {
         bind = [
           "super, return, exec, alacritty"
           "super shift, q, killactive"
-          "super, t, togglefloating"
           "super, o, exec, bemenu-run"
 
           "super, h, movefocus, l"
@@ -20,27 +27,8 @@
           "super, k, movefocus, u"
           "super, j, movefocus, d"
 
-          "super, 1, workspace, 1"
-          "super, 2, workspace, 2"
-          "super, 3, workspace, 3"
-          "super, 4, workspace, 4"
-          "super, 5, workspace, 5"
-          "super, 6, workspace, 6"
-          "super, 7, workspace, 7"
-          "super, 8, workspace, 8"
-          "super, 9, workspace, 9"
-          "super, 0, workspace, 10"
-
-          "super shift, 1, movetoworkspace, 1"
-          "super shift, 2, movetoworkspace, 2"
-          "super shift, 3, movetoworkspace, 3"
-          "super shift, 4, movetoworkspace, 4"
-          "super shift, 5, movetoworkspace, 5"
-          "super shift, 6, movetoworkspace, 6"
-          "super shift, 7, movetoworkspace, 7"
-          "super shift, 8, movetoworkspace, 8"
-          "super shift, 9, movetoworkspace, 9"
-          "super shift, 0, movetoworkspace, 10"
+          "super, t, workspace, name:terminal"
+          "super, b, workspace, name:browser"
 
           "super, S, togglespecialworkspace, magic"
           "super shift, S, movetoworkspace, special:magic"
@@ -63,8 +51,8 @@
 
 
       general = {
-        gaps_in = 5;
-        gaps_out = 5;
+        gaps_in = 3;
+        gaps_out = 6;
 
         border_size = 2;
 
@@ -80,8 +68,8 @@
 
         blur = {
           enabled = true;
-          size = 5;
-          passes = 1;
+          size = 3;
+          passes = 3;
         };
       };
 
