25755c5dc9bf6752f8ba36373c75cac4ccb82621
RicoProductions <ericravaz@protonmail.com>
2024-05-09 14:14:33 +0200

    add waybar style

--- home/waybar/waybar.nix
+++ home/waybar/waybar.nix
@@ -2,13 +2,35 @@
 
   programs.waybar = {
     enable = true;
+    systemd.enable = true;
 
     settings = {
-      layer = "top";
-      position = "bottom";
-      height = 34;
+      bar = {
+        layer = "top";
+        position = "bottom";
+        height = 34;
 
-      modules-left = [ "hyprland/workspace" ];
+        modules-left = [ "hyprland/workspaces" ];
+      };
     };
+
+    style = with config.colors; ''
+      :root {
+        --bg0: #${bg0};
+        --bg1: #${bg1};
+        --fg: #${fg};
+
+        --black: #${black};
+        --gray: #${gray};
+        --red: #${red};
+        --yellow: #${yellow};
+        --green: #${green};
+        --cyan: #${cyan};
+        --blue: #${blue};
+        --magenta: #${magenta};
+      }
+
+      ${builtins.readFile ./waybar.css}
+    '';
   };
-};
+}
