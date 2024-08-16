2faefe4a58c51d83cc5756e40344e5f1569d3d79
RicoProductions <ericravaz@protonmail.com>
2024-08-16 19:45:40 +0200

    set cursor timeout

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -74,6 +74,10 @@ in {
         ];
       };
 
+      cursor = {
+        inactive_timeout = 17;
+      };
+
       misc = {
         force_default_wallpaper = 0;
         disable_hyprland_logo = true;
