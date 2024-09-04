61c8d03a06c2fe8f6264659bd0278f0444e79570
RicoProductions <ericravaz@protonmail.com>
2024-09-04 16:34:41 +0200

    fix typo

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "RicOS config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24-05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
