2e78df38bd559dc4fea8b5203cef3169354815ee
RicoProductions <ericravaz@protonmail.com>
2024-09-04 16:26:34 +0200

    fix declaration priorites

--- hosts/live-image/default.nix
+++ hosts/live-image/default.nix
@@ -1,6 +1,11 @@
-{ modulesPath, ... }: {
+{ modulesPath, lib, pkgs, ... }: {
 
   imports = [
     (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
   ];
+
+  boot = {
+    kernelPackages = lib.mkForce pkgs.linuxPackages;
+    loader.timeout = lib.mkForce 10;
+  };
 }
