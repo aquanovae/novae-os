89cf1daba8d6427b196baa9660aafe043cdf5c8b
RicoProductions <ericravaz@protonmail.com>
2024-09-10 01:52:18 +0200

    switch to iwd

--- modules/core/wireless.nix
+++ modules/core/wireless.nix
@@ -1,10 +1,14 @@
 { ... }: {
 
-  networking.wireless = {
+  networking.wireless.iwd = {
     enable = true;
-    networks = {
-      alfred = {
-        pskRaw = "1b753ca34af52105bb2454cf997889b639e161dc2d851be0c58b450610e63c4c";
+    settings = {
+      Security = {
+        PreSharedKey = "1b753ca34af52105bb2454cf997889b639e161dc2d851be0c58b450610e63c4c";
+      };
+
+      Settings = {
+        AutoConnect = true;
       };
     };
   };
