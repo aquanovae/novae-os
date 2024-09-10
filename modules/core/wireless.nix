39e0c30320b50d3e8cf195da6c9730e467cf6b46
RicoProductions <ericravaz@protonmail.com>
2024-09-10 02:36:29 +0200

    remove non working auto connection

--- modules/core/wireless.nix
+++ modules/core/wireless.nix
@@ -1,15 +1,4 @@
 { ... }: {
 
-  networking.wireless.iwd = {
-    enable = true;
-    settings = {
-      Security = {
-        PreSharedKey = "1b753ca34af52105bb2454cf997889b639e161dc2d851be0c58b450610e63c4c";
-      };
-
-      Settings = {
-        AutoConnect = true;
-      };
-    };
-  };
+  networking.wireless.iwd.enable = true;
 }
