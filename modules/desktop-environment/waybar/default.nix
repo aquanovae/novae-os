5d859aecd1e7155d5124fcc1322a0e6145f7270e
RicoProductions <ericravaz@protonmail.com>
2024-09-12 21:15:52 +0200

    restructure waybar config

--- modules/desktop-environment/waybar/default.nix
+++ modules/desktop-environment/waybar/default.nix
@@ -1,7 +1,42 @@
-{ de-setup, username, ... }: {
+{ hostname, lib, username, ... }:
+
+let
+  modules-left = [
+    "custom/os-icon"
+    "hyprland/workspaces"
+    "hyprland/submap"
+  ];
+
+  modules-center = lib.optionals (
+    (hostname == "silverlight") || 
+    (hostname == "zenblade")
+  ) [
+    "custom/playerctl-info"
+  ];
+
+  modules-right = lib.optionals (hostname == "silverlight") [
+    "custom/shutdowntime"
+    "disk"
+    "memory"
+    "cpu"
+    "custom/gpu-info"
+    "clock"
+
+  ] ++ lib.optionals (hostname == "zenblade") [
+    "custom/shutdowntime"
+    "volume"
+    "disk"
+    "memory"
+    "cpu"
+    "battery"
+    "clock"
+
+  ] ++ lib.optionals (hostname == "live-iso") [
+    "clock"
+  ];
+in {
 
   imports = [
-    ./${de-setup}.nix
     ./modules.nix
     ./style.nix
   ];
@@ -16,6 +51,10 @@
       margin = "0px 6px 6px";
       spacing = 7;
       reload_style_on_change = true;
+
+      modules-left = modules-left;
+      modules-center = modules-center;
+      modules-right = modules-right;
     };
   };
 }
