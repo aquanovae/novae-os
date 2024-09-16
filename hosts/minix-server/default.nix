7cb1873114828a1a52a6fe3c4145d0c0ea82acd0
RicoProductions <ericravaz@protonmail.com>
2024-09-16 18:19:13 +0200

    add hardware config and use normal kernel for server

--- hosts/minix-server/default.nix
+++ hosts/minix-server/default.nix
@@ -1,8 +1,10 @@
-{ ... }: {
+{ lib, pkgs, ... }: {
 
   imports = [
     ./hardware-configuration.nix
   ];
+
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
 
   system.stateVersion = "24.05";
 };
