4767d96654d72316e786a4184d368ebe84c4f842
RicoProductions <ericravaz@protonmail.com>
2024-09-16 18:36:25 +0200

    add ricos config

--- hosts/minix-server/default.nix
+++ hosts/minix-server/default.nix
@@ -4,7 +4,24 @@
     ./hardware-configuration.nix
   ];
 
+  ricos = {
+    desktopEnvironment.enable = false;
+
+    programs = {
+      coolercontrol.enable = false;
+      defaultDesktopApps.enable = false;
+      gamingApps.enable = false;
+      imageEditingApps.enable = false;
+      openrgb.enable = false;
+      virtualisationApps.enable = false;
+    };
+
+    server = {
+      ssh.enable = true;
+    };
+  };
+
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages;
 
   system.stateVersion = "24.05";
-};
+}
