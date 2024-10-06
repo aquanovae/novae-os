2830019686dca9ed67ce60c0d3094a213748b398
RicoProductions <ericravaz@protonmail.com>
2024-10-06 20:46:45 +0200

    static ip for server

---
+++ modules/server/static-ip.nix
@@ -0,0 +1,20 @@
+{ config, lib, ... }:
+
+let
+  interface = "enp1s0";
+in {
+
+  config = lib.mkIf config.server.enable {
+    networking = {
+      interfaces.${interface}.ipv4.addresses = [{
+        address = "10.7.7.7";
+        prefixLength = 24;
+      }];
+
+      defaultGateway = {
+        address = "10.7.7.1";
+        interface = interface;
+      };
+    };
+  };
+}
