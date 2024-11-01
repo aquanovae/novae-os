10b5f8a69e6fca518045d0bad92b716228882fd6
RicoProductions <ericravaz@protonmail.com>
2024-11-01 19:12:21 +0100

    lockscreen theming

--- modules/desktop-environment/hyprlock.nix
+++ modules/desktop-environment/hyprlock.nix
@@ -12,25 +12,69 @@ in {
           hide_cursor = true;
         };
 
-        background = [{
-          monitor = "";
-          path = "/home/rico/.config/hypr/wallpaper.png";
-          blur_passes = 3;
-          blur_size = 5;
-        }];
+        background = [
+          { monitor = "";
+            path = "/home/rico/.config/hypr/wallpaper.png";
+            blur_passes = 3;
+            blur_size = 5;
+          }
+        ];
 
-        input-field = [{
-          monitor = "";
-          position = "300, 0";
-          halign = "left";
-          valign = "center";
-          size = "200, 30";
-          outline_thickness = 2;
-          rounding = 5;
-          outer_color = "rgb(${theme.blue})";
-          inner_color = "rgb(${theme.bg0})";
-          fade_on_empty = false;
-        }];
+        shape = [
+          # Box around usernam and input field
+          { monitor = "";
+            size = "260, 90";
+            position = "200, 0";
+            halign = "left";
+            valign = "center";
+            border_size = 2;
+            rounding = 5;
+            color = "rgb(${theme.bg0})";
+            border_color = "rgb(${theme.bg2})";
+          }
+        ];
+
+        label = [
+          # Username
+          { monitor = "";
+            position = "220, 23";
+            halign = "left";
+            valign = "center";
+            text = "$USER";
+            font_family = "JetBrainsMono Nerd Font";
+            font_size = "13";
+          }
+
+          # Time
+          { monitor = "";
+            position = "200, 130";
+            halign = "left";
+            valign = "center";
+            text = "$TIME";
+            font_family = "JetBrainsMono Nerd Font Bold";
+            font_size = "47";
+          }
+        ];
+
+        input-field = [
+          { monitor = "";
+            size = "200, 30";
+            position = "220, -10";
+            halign = "left";
+            valign = "center";
+            outline_thickness = 2;
+            rounding = 5;
+            outer_color = "rgb(${theme.blue})";
+            inner_color = "rgb(${theme.bg3})";
+            fail_color = "rgb(${theme.red})";
+            check_color = "rgb(${theme.magenta})";
+            font_color = "rgb(${theme.fg})";
+            capslock_color = "rgb(${theme.red})";
+            fade_on_empty = false;
+            placeholder_text = "";
+            fail_text = "";
+          }
+        ];
       };
     };
   };
