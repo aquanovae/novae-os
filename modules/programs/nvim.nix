692fb706945935caeebd03c4d9b476cc576823a8
RicoProductions <ericravaz@protonmail.com>
2024-11-12 21:02:32 +0100

    add C lsp to nvim

--- modules/programs/nvim.nix
+++ modules/programs/nvim.nix
@@ -21,21 +21,26 @@
 
     # Programs required by plugins
     extraPackages = with pkgs; [
+      ccls
       nixd
       rust-analyzer
     ];
 
+    # Nvim config
     extraLuaConfig = /*lua*/ ''
       local o = vim.o
       local opt = vim.opt
 
+      -- Syntax
       opt.syntax = enable
       opt.termguicolors = true
 
+      -- Current line information
       opt.number = true
       opt.relativenumber = true
       opt.cursorline = true
 
+      -- Indentation
       opt.tabstop = 2
       opt.shiftwidth = 2
       opt.softtabstop = 2
@@ -43,15 +48,19 @@
       opt.autoindent = true
       opt.smartindent = true
 
+      -- Search highlight
       opt.hlsearch = false
       opt.incsearch = true
 
+      -- 
       opt.scrolloff = 13
       opt.wrap = false
 
+      -- Key bindings
       vim.g.mapleader = " "
       vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
+      -- Theme
       require("onedark").setup {
         style = "darker",
         transparent = true,
@@ -59,12 +68,14 @@
       }
       require("onedark").load()
 
+      -- Indentation lines
       require("ibl").setup {
         scope = {
           enabled = false,
         },
       }
 
+      -- Syntax highlighting
       require("treesitter-context").setup()
       require("nvim-treesitter.configs").setup {
         auto_install = false,
@@ -74,14 +85,17 @@
         },
       }
 
+      -- Fuzzy file finder
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>s", builtin.find_files, {})
 
+      -- LSP
       require("lsp-zero")
       require("lspconfig").nixd.setup {}
-      --require("otter").activate("rust", true, true, nil)
+      require("lspconfig").ccls.setup {}
     '';
 
+    # Make nvim default
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
