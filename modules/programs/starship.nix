d4fe5da620cea7bee592b7b77bed564085ce110f
RicoProductions <ericravaz@protonmail.com>
2024-12-11 21:10:35 +0100

    add space after prompt

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -62,7 +62,7 @@
       };
 
       character = {
-        format = "$symbol";
+        format = "$symbol ";
         success_symbol = "[>>>](bold green)";
         error_symbol = "[>>>](bold red)";
         vicmd_symbol = "[<<<](bold purple)";
