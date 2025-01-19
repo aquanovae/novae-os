b8b6f7a5081b228aca25fed281adafaf8dc04545
aquanovae <ericravaz@protonmail.com>
2025-01-19 21:39:38 +0100

    file formating

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -1,7 +1,7 @@
 # ------------------------------------------------------------------------------
 # Shell configuration
 # ------------------------------------------------------------------------------
-{ config, username, ... }: let
+{ username, ... }: let
 
   novae-os = "/home/${username}/novae-os/";
 
@@ -16,48 +16,48 @@ in {
 
     shellAliases = {
       # Aliases for cargo
-      cb = "cargo build";
-      cr = "cargo run";
+      cb      = "cargo build";
+      cr      = "cargo run";
 
       # Aliases for git
-      ga = "git add";
-      gb = "git branch";
-      gcm = "git commit -m";
-      gd = "git diff";
-      gf = "git fetch";
-      gl = "git log --oneline --graph --all -20";
-      gll = "git log --oneline --graph --all";
-      gm = "git merge";
-      gmv = "git mv";
-      gp = "git push";
-      gpl = "git pull";
-      gr = "git rebase";
-      grm = "git rm";
-      grstr = "git restore";
-      gs = "git status";
-      gst = "git stash";
-      gstl = "git stash list";
-      gstp = "git stash pop";
-      gsw = "git switch";
+      ga      = "git add";
+      gb      = "git branch";
+      gcm     = "git commit -m";
+      gd      = "git diff";
+      gf      = "git fetch";
+      gl      = "git log --oneline --graph --all -20";
+      gll     = "git log --oneline --graph --all";
+      gm      = "git merge";
+      gmv     = "git mv";
+      gp      = "git push";
+      gpl     = "git pull";
+      gr      = "git rebase";
+      grm     = "git rm";
+      grstr   = "git restore";
+      gs      = "git status";
+      gst     = "git stash";
+      gstl    = "git stash list";
+      gstp    = "git stash pop";
+      gsw     = "git switch";
 
       # Aliases for ls
-      l = "ls -l --color=auto";
-      ll = "ls -la --color=auto";
+      l       = "ls -l --color=auto";
+      ll      = "ls -la --color=auto";
 
       # Alias to run nix-shell with zsh
-      ns = "nix-shell --run zsh";
-      nsp = "nix-shell --run zsh -p";
+      ns      = "nix-shell --run zsh";
+      nsp     = "nix-shell --run zsh -p";
 
       # Aliases for system management
-      nbi = "sudo nix build --impure ${novae-os}/#nixosConfigurations.live-image.config.system.build.isoImage";
-      ncg = "sudo nix-collect-garbage --delete-older-than 3d";
-      nfu = "nix flake update --flake ${novae-os}";
-      nrs = "sudo nixos-rebuild switch --impure --flake ${novae-os}";
+      nbi     = "sudo nix build --impure ${novae-os}/#nixosConfigurations.live-image.config.system.build.isoImage";
+      ncg     = "sudo nix-collect-garbage --delete-older-than 3d";
+      nfu     = "nix flake update --flake ${novae-os}";
+      nrs     = "sudo nixos-rebuild switch --impure --flake ${novae-os}";
+      nsg     = "l /nix/store | grep -i";
 
       # Other aliases
-      ".." = "cd ..";
-      nsg = "l /nix/store | grep -i";
-      tree = "tree --dirsfirst";
+      ".."    = "cd ..";
+      tree    = "tree --dirsfirst";
 
     };
   };
