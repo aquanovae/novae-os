97d71b97b9d901ae8ced00716821ee0a2a606e3f
aquanovae <ericravaz@protonmail.com>
2025-02-13 22:04:50 +0100

    update

--- hosts/zenblade/default.nix
+++ hosts/zenblade/default.nix
@@ -30,19 +30,19 @@
       vscode.enable = true;
     };
 
+    /*
     virtualMachine = {
       enable = true;
       coreCount = "12";
       memory = "10G";
-      /*
       gpuPassthrough = {
         enable = true;
         gpuId = "10de:25a0";
         gpuPciId = "01:00.0";
         fakeBattery.enable = true;
       };
-      */
     };
+    */
   };
 
   system.stateVersion = "23.11";
