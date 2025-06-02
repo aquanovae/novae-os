73d8c760f3ec6d2186d7daab11085b1cf55b3fc5
aquanovae <ericravaz@protonmail.com>
2025-06-02 18:35:05 +0200

    fix rtc

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -33,8 +33,5 @@
   i18n.defaultLocale = "en_GB.UTF-8";
   console.keyMap = "fr_CH";
 
-  time = {
-    timeZone = "Europe/Zurich";
-    hardwareClockInLocalTime = true;
-  };
+  time.timeZone = "Europe/Zurich";
 }
