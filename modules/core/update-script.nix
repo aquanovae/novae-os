bcf8212519d443f9fcd112af5034441d9a3c55cc
aquanovae <ericravaz@protonmail.com>
2025-09-14 08:17:18 +0200

    small fix

--- modules/core/update-script.nix
+++ modules/core/update-script.nix
@@ -6,9 +6,9 @@
     git fetch && \
     git pull --rebase && \
     echo -e "\e[1;32m-> \e[37mUpdating flake\e[0m" && \
-    nix flake update --flake /home/${username}/novae-os 2>&1 | grep "Updated" | sed -e "s/• //" -e "s/://" && \
+    nix flake update --flake . 2>&1 | grep "Updated" | sed -e "s/• //" -e "s/://" && \
     echo -e "\n\e[1;32m-> \e[37mBuilding configuration\e[0m" && \
-    sudo nixos-rebuild switch --impure --flake /home/${username}/novae-os --log-format bar && \
+    sudo nixos-rebuild switch --impure --flake . --log-format bar && \
     echo -e "\n\e[1;32m-> \e[37mCleaning store\e[0m" && \
     echo -e "\e[1mSystem space:\e[0m" && \
     sudo nix-collect-garbage --delete-older-than 3d 2>&1 | grep "store paths deleted" && \
