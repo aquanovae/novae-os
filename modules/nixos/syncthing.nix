977aabce3ac4189cdce8c062e53f193928034c92
aquanovae <ericravaz@protonmail.com>
2026-03-12 18:30:50 +0100

    add silverlight

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -12,13 +12,14 @@
 
     services.syncthing.settings = {
       devices = {
-        minix =     { id = "T3XODJT-G2M6DBY-MUAWXK6-MF3OEUX-A7T3KUA-TETKBSU-RCTPAXS-22EGNAC"; };
-        zenblade =  { id = "E6WVNQ2-VBIOLUC-IJRBDGY-TSEJWFQ-FAHAKG6-6Q37B65-PTUVJUO-GQP6VQY"; };
+        minix       = { id = "T3XODJT-G2M6DBY-MUAWXK6-MF3OEUX-A7T3KUA-TETKBSU-RCTPAXS-22EGNAC"; };
+        silverlight = { id = "DD6KA6S-2DT7NGY-GJZFCEI-MZP7PQX-KBCDTXM-6XEAGKM-RSEUQO6-CXWMUAW"; };
+        zenblade    = { id = "E6WVNQ2-VBIOLUC-IJRBDGY-TSEJWFQ-FAHAKG6-6Q37B65-PTUVJUO-GQP6VQY"; };
       };
 
       folders.files = {
         path = "/home/aquanovae/files";
-        devices = [ "minix" "zenblade" ];
+        devices = [ "minix" "silverlight" "zenblade" ];
       };
     };
   };
